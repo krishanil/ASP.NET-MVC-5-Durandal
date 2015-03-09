69e39de7741e2c2db63117b82305c43895335257
Alexey Voroshak <alexey.vorohsak@nat.kz>
2015-03-09 20:31:16 +0600

    Implemented View Data extention

---
+++ WebApplication/WebApplication/Utils/Extentions/ViewDataExtentions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace WebApplication.Utils.Extentions
+{
+    public static class ViewDataExtentions
+    {
+        public static string AdditionalData(this ViewDataDictionary viewData)
+        {
+            var builder = new StringBuilder();
+            viewData.ToList().ForEach(d => builder.Append(string.Format("{0}={1}", d.Key == "data_bind" ? "data-bind" : d.Key, d.Value.ToString().Replace(" ", string.Empty))));
+            return builder.ToString();
+        }
+    }
+}
