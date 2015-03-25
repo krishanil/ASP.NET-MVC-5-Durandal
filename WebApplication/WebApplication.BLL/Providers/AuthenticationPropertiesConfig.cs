5347659495d74e9e74846d6b6573ce8718ad88b3
Alexey Voroshak <alexey.vorohsak@nat.kz>
2015-03-25 18:06:59 +0600

    Changed ApplicationOAuthProvider

--- WebApplication/WebApplication.BLL/Providers/AuthenticationPropertiesConfig.cs
+++ WebApplication/WebApplication.BLL/Providers/AuthenticationPropertiesConfig.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using WebApplication.BLL.Managers.Account;
 
 namespace WebApplication.BLL.Providers
 {
@@ -22,5 +25,29 @@ namespace WebApplication.BLL.Providers
 
             return new AuthenticationProperties(data);
         }
+
+        public static async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context, AppUserManager userManager, string cookie)
+        {
+            var user = await userManager.FindAsync(context.UserName, context.Password);
+
+            if (user == null)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+
+            var oAuthIdentity = await user.GenerateUserIdentityAsync(userManager, OAuthDefaults.AuthenticationType);
+
+            var cookiesIdentity =
+                await user.GenerateUserIdentityAsync(userManager, cookie);
+
+            var properties = CreateProperties(oAuthIdentity);
+            var ticket = new AuthenticationTicket(oAuthIdentity, properties);
+
+            context.Validated(ticket);
+            context.Request.Context.Authentication.SignIn(cookiesIdentity);
+        }
     }
+
+
 }
--- WebApplication/WebApplication/Providers/ApplicationOAuthProvider.cs
+++ WebApplication/WebApplication/Providers/ApplicationOAuthProvider.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
 using WebApplication.BLL.Managers.Account;
@@ -26,25 +25,8 @@ namespace WebApplication.Providers
         public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
             var userManager = context.OwinContext.GetUserManager<AppUserManager>();
-
-            var user = await userManager.FindAsync(context.UserName, context.Password);
-
-            if (user == null)
-            {
-                context.SetError("invalid_grant", "The user name or password is incorrect.");
-                return;
-            }
-
-            var oAuthIdentity = await user.GenerateUserIdentityAsync(userManager, OAuthDefaults.AuthenticationType);
-
-            var cookiesIdentity = await user.GenerateUserIdentityAsync(userManager, CookieAuthenticationDefaults.AuthenticationType);
-
-            var properties = AuthenticationPropertiesConfig.CreateProperties(oAuthIdentity);
-            var ticket = new AuthenticationTicket(oAuthIdentity, properties);
-            
-            context.Validated(ticket);
-            context.Request.Context.Authentication.SignIn(cookiesIdentity);
-        }
+            await AuthenticationPropertiesConfig.GrantResourceOwnerCredentials(context, userManager, CookieAuthenticationDefaults.AuthenticationType);
+        }        
 
         public override Task TokenEndpoint(OAuthTokenEndpointContext context)
         {
